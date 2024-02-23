6fba7b065883b82c60de04e8b0ae26bb2ba905c1
Sam Miller <millersamu@uwplatt.edu>
2024-02-23 15:32:24 -0600

    hw1

---
+++ HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
