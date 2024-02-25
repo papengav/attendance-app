7a5def96f28641aa8d185f3e5ad437b126358559
Sam Miller <millersamu@uwplatt.edu>
2024-02-25 16:41:00 -0600

    Login UI without API endpoint functionality

---
+++ loginfrontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Appbar from './components/Appbar'
+import Userlogin from './components/User'
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <Userlogin/>
+    </div>
+  );
+}
+
+export default App;
