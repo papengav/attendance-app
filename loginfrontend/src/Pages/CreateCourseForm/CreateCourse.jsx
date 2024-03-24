67b4390421e5e70f9bf777db75cb8ba05a8e465a
Sam Miller <millers.j03@gmail.com>
2024-03-24 14:30:25 -0500

    Added styling to create course and create user UI pages. Testing React Date-Time-Local for tracking meeting times

--- loginfrontend/src/Pages/CreateCourseForm/CreateCourse.jsx
+++ loginfrontend/src/Pages/CreateCourseForm/CreateCourse.jsx
@@ -5,18 +5,18 @@ import React, { useState } from 'react';
 import './CreateCourse.css';
 
 const CreateCourse = () => {
-    const [courseName, setCourseName] = useState("")
-    const [numSections, setNumSections] = useState(1)
-    const [professorName, setProfessorName] = useState("")
-    const changeCourseName = event => {
-        setCourseName(event.target.value)
-    }
-    const changeNumSections = event => {
-        setNumSections(event.target.value)
-    }
-    const changeProfessorName = event => {
-        setProfessorName(event.target.value)
-    }
+    const [courseName, setCourseName] = useState('')
+    const [numSections, setNumSections] = useState()
+    const [professorName, setProfessorName] = useState('')
+    // const changeCourseName = event => {
+    //     setCourseName(event.target.value)
+    // }
+    // const changeNumSections = event => {
+    //     setNumSections(event.target.value)
+    // }
+    // const changeProfessorName = event => {
+    //     setProfessorName(event.target.value)
+    // }
     const handleClick = (e) => {
         e.preventDefault();
         const course = {courseName, numSections, professorName}
@@ -31,23 +31,58 @@ const CreateCourse = () => {
         });
     };
     return (
-        <div className="d-flex justify-content-center mt-5">
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Course Name</h4>
-                <input onChange={changeCourseName} value = {courseName}/>
-            </div>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Number of Sections</h4>
-                <input onChange={changeNumSections} value = {numSections}/>
-            </div>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Professor Name</h4>
-                <input onChange={changeProfessorName} value = {professorName}/>
-            </div>
-            <div>
-                <button onClick={handleClick}>Submit</button>
-            </div>
+        <div className='wrapper'>
+            <form onSubmit={handleClick}>
+                <h1>Create Course</h1>
+                <div className='input-box'>
+                    <input
+                    type= "text"
+                    placeholder='Course Name'
+                    value={courseName}
+                    onChange= {(e) => setCourseName(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                    type= "number"
+                    placeholder='1'
+                    value={numSections}
+                    onChange={(e) => setNumSections(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                    type= "text"
+                    placeholder='Professor Name'
+                    value={professorName}
+                    onChange={(e) => setProfessorName(e.target.value)}
+                    required
+                    />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
+        //this is the old implementation pre-styling
+
+        // <div className="d-flex justify-content-center mt-5">
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Course Name</h4>
+        //         <input onChange={changeCourseName} value = {courseName}/>
+        //     </div>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Number of Sections</h4>
+        //         <input onChange={changeNumSections} value = {numSections}/>
+        //     </div>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Professor Name</h4>
+        //         <input onChange={changeProfessorName} value = {professorName}/>
+        //     </div>
+        //     <div>
+        //         <button onClick={handleClick}>Submit</button>
+        //     </div>
+        // </div>
     );
 };
 
--- loginfrontend/src/Pages/CreateSectionForm/CreateSection.jsx
+++ loginfrontend/src/Pages/CreateSectionForm/CreateSection.jsx
@@ -5,22 +5,22 @@ import React, { useState } from 'react';
 import './CreateSection.css';
 
 const CreateSection = () => {
-    const [roomNumber, setRoomNumber] = useState(0)
+    const [roomNumber, setRoomNumber] = useState()
     const [startTime, setStartTime] = useState("")
     const [endTime, setEndTime] = useState("")
-    const [numStudents, setNumStudents] = useState(0)
-    const changeRoomNumber = event => {
-        setRoomNumber(event.target.value)
-    }
-    const changeStartTime = event => {
-        setStartTime(event.target.value)
-    }
-    const changeEndTime = event => {
-        setEndTime(event.target.value)
-    }
-    const changeNumStudents = event => {
-        setNumStudents(event.target.value)
-    }
+    const [numStudents, setNumStudents] = useState()
+    // const changeRoomNumber = event => {
+    //     setRoomNumber(event.target.value)
+    // }
+    // const changeStartTime = event => {
+    //     setStartTime(event.target.value)
+    // }
+    // const changeEndTime = event => {
+    //     setEndTime(event.target.value)
+    // }
+    // const changeNumStudents = event => {
+    //     setNumStudents(event.target.value)
+    // }
     const handleClick = (e) => {
         e.preventDefault();
         const section = {roomNumber, startTime, endTime, numStudents}
@@ -36,27 +36,72 @@ const CreateSection = () => {
     };
 
     return (
-        <div className='d-flex justify-content-center mt-5'>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Section Room Number</h4>
-                <input onChange={changeRoomNumber} value= {roomNumber}/>
-            </div>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Section Start Time</h4>
-                <input onChange={changeStartTime} value = {startTime}/>
-            </div>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Section End Time</h4>
-                <input onChange={changeEndTime} value = {endTime}/>
-            </div>
-            <div className='w-50 p-3 border rounded'>
-                <h4>Input Number of Students</h4>
-                <input onChange={changeNumStudents} value = {numStudents}/>
-            </div>
-            <div>
-                <button onClick={handleClick}>Submit</button>
-            </div>
+        <div className='wrapper'>
+            <form onSubmit={handleClick}>
+                <h1>Create Section</h1>
+                <div className='input-box'>
+                    <input
+                    type= "text"
+                    placeholder='001A'
+                    value={roomNumber}
+                    onChange={(e) => setRoomNumber(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                    type= "datetime-local"
+                    placeholder='1:00 pm'
+                    value={startTime}
+                    onChange={(e) => setStartTime(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                    type = "datetime-local"
+                    placeholder='1:50 pm'
+                    value={endTime}
+                    onChange={(e) => setEndTime(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className='input-box'>
+                    <input
+                    type= "number"
+                    placeholder='1'
+                    value={numStudents}
+                    onChange={(e) => setNumStudents(e.target.value)}
+                    required
+                    />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
+
+        //This is the old implementation pre-styling
+
+        // <div className='d-flex justify-content-center mt-5'>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Section Room Number</h4>
+        //         <input onChange={changeRoomNumber} value= {roomNumber}/>
+        //     </div>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Section Start Time</h4>
+        //         <input onChange={changeStartTime} value = {startTime}/>
+        //     </div>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Section End Time</h4>
+        //         <input onChange={changeEndTime} value = {endTime}/>
+        //     </div>
+        //     <div className='w-50 p-3 border rounded'>
+        //         <h4>Input Number of Students</h4>
+        //         <input onChange={changeNumStudents} value = {numStudents}/>
+        //     </div>
+        //     <div>
+        //         <button onClick={handleClick}>Submit</button>
+        //     </div>
+        // </div>
     );
 };
 
